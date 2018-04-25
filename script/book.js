ea65b1d5a7e425ccd93f393237a150a30a58af21
Rhiannon98 <mortensenrhi@gmail.com>
2018-04-25 14:35:58 -0700

    spacing fixes

--- script/book.js
+++ script/book.js
@@ -4,55 +4,55 @@ let app = {}
 
 (function (module) {
 
-  function Book(obj){
+  function Book(obj) {
     Object.assign(this, obj);
-  };
+  }
 
   Book.all = [];
 
-  Book.prototype.toHtml = function(){
+  Book.prototype.toHtml = function () {
     let template = $('#book-template').html();
     let bookTemplate = Handlebars.compile(template);
     return bookTemplate(this);
   };
 
   Book.loadAll = JSONData => {
-    Book.all = JSONData.map ( e => new Book(e));
+    Book.all = JSONData.map(e => new Book(e));
   };
 
   Book.fetchAll = callback => {
     $.get('/books')
-      .then( results => {
+      .then(results => {
         Book.loadAll(results);
         callback();
-      })
-  }
+      });
+  };
 
   Book.truncateTable = callback => {
     $.ajax({
       url: '/books',
       method: 'DELETE'
     })
-    .then(console.log)
-    .then(callback);
-  }
+      .then(console.log)
+      .then(callback);
+  };
 
   Book.prototype.insertRecord = callback => {
     let insertionObject = {};
     Object.assign(insertionObject, this);
     $.post('/books', insertionObject)
-    .then(console.log)
-    .then(callback);
-  }
+      .then(console.log)
+      .then(callback);
+  };
 
   Book.prototype.deleteRecord = callback => {
     $.ajax({
       url: `/books/${this.book_id}`,
       method: 'DELETE'
     })
-    .then(console.log)
-    .then(callback)
-  }
+      .then(console.log)
+      .then(callback);
+  };
 
   Book.prototype.updateRecord = callback => {
     let updateObject = {};
@@ -60,10 +60,10 @@ let app = {}
     $.ajax({
       url: `/books/${this.book_id}`,
       method: 'PUT',
-      data : updateObject
-    })
-  }
+      data: updateObject
+    });
+  };
 
   module.Book = Book;
 
-})(app)
+})(app);
